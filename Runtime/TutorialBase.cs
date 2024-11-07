36a0ef075e56a2413232f76a9bc300ce4f7bf4a2
Weackss <62299994+UBTA@users.noreply.github.com>
2024-11-07 19:33:28 +0300

    1.1.0

---
+++ Runtime/TutorialBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace EblanDev.ScenarioCore.Systems.TutorialSystemUnit
+{
+    public abstract class TutorialBase : MonoBehaviour
+    {
+        public Action<TutorialID> OnTutorPassed;
+        
+        public TutorialID ID;
+        public bool IsActive => isActive;
+
+        protected TutorialCanvas tutorialCanvas;
+        protected bool isActive;
+
+        public void Init(TutorialCanvas canvas)
+        {
+            tutorialCanvas = canvas;
+        }
+        public abstract void Deactivate();
+    }
+}
---
+++ Runtime/TutorialData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using EblanDev.ScenarioCore.GameFramework.Data;
+using EblanDev.ScenarioCore.UtilsFramework.Extensions;
+using UnityEngine;
+
+namespace EblanDev.ScenarioCore.Systems.TutorialSystemUnit
+{
+    [CreateAssetMenu(fileName = "TutorialData", menuName = "ScenarioFramework/Systems/TutorialData", order = 1)]
+    public class TutorialData : DataPrefs<TutorialPrefs>
+    {
+        public void SetTutorialPassed(TutorialID tutorID)
+        {
+            if (PData.passedTutorialIds.TryFind((v) => tutorID == v, out var id))
+            {
+                return;
+            }
+            
+            PData.passedTutorialIds.Add(tutorID);
+        }
+
+        public bool IsTutorialPassed(TutorialID tutorID)
+        {
+            return PData.passedTutorialIds.TryFind((v) => tutorID == v, out var id);
+        }
+    }
+
+    [Serializable]
+    public class TutorialPrefs
+    {
+        public List<int> passedTutorialIds;
+
+        public TutorialPrefs()
+        {
+            passedTutorialIds = new List<int>();
+        }
+    }
+}
